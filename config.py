bb659707c90c632d29d28f86578c6f6ea4e68905
Ilnaz Savinov <izsavinov@edu.hse.ru>
2022-04-02 18:07:52 +0300

    Changed docstrings

--- config.py
+++ config.py
@@ -4,10 +4,12 @@ from os import environ
 
 class Config:
     def __init__(self, filename):
+        """Формирование словаря с конфигурацией из файла и переменных среды."""
         with open(filename) as f:
             self.config = json.load(f)
         self.config = environ | self.config
 
     def dump(self, filename):
+        """Выгрузка конфигурации в .json-файл."""
         with open(filename, 'w+') as f:
             json.dump(self.config, f)
