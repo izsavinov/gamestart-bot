3b523293ad94fd629be4c0d1892ca27dff5f729c
Ilnaz Savinov <izsavinov@edu.hse.ru>
2022-04-07 11:15:11 +0300

    adding a file bot_classes.py, adding functions to bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 from discord.ext import commands
+import datetime
+import asyncio
+from bot_classes import statsdata
 from os import environ
 from sys import argv
-
 from config import Config
 
+nickFI = ''
+nickSteam = ''
 
 config = Config(argv[1]).config
 client = commands.Bot(command_prefix=config['prefix'])
@@ -19,6 +23,47 @@ async def on_ready():
 async def hello(ctx):
     """Приветствие пользователя по команде в Discord."""
     await ctx.send(config['greeting'])
+
+
+@client.command(pass_context=True)
+async def reminder(ctx, message: str):
+    """Напоминалка о начале матча, на вход передается день, часы, минуты, секунды начала матча. Сообщение о начале
+    матча приходит за 15 минут и в момент, на который была запланирована игра"""
+    datepattern = '%d-%H:%M:%S'  # Строковый тип
+    gamedata = message
+    time_now = datetime.datetime.now()
+    try:
+        date = datetime.datetime.strptime(gamedata, datepattern)
+    except:
+        pass
+    deltadate = (date.day * 24 * 3600 + date.hour * 3600 + date.minute * 60 + date.second) - (time_now.day * 24 * 3600 \
+                                                                                              + time_now.hour * 3600 + time_now.minute * 60 + time_now.second)
+    if (deltadate - 15 * 60 > 0):
+        await asyncio.sleep(deltadate - 15 * 60 + 2)
+        await ctx.send('15 минут')
+    else:
+        await asyncio.sleep(deltadate)
+        await ctx.send('Start')
+
+
+@client.command(pass_context=True)
+async def getnickfi(ctx, message1: str, message2: str):
+    """Пользователь предоставляет нам информацию о своем аккаунте FACEIT, а именно - никнейм
+    Пользователь предоставляет нам информацию о своем аккаунте Steam, а именно - никнейм"""
+    global nickFI
+    global nickSteam
+    nickFI = message1
+    nickSteam = message2
+    await ctx.send('{} и {}'.format(nickFI, nickSteam))
+
+
+@client.command(pass_context=True)
+async def statistica(ctx):
+    """Выводит статистику"""
+    statsdata_obj = statsdata(config['APPID'], config['TOKEN'])
+    player_id = statsdata.player_details(statsdata_obj, nickFI, nickSteam)
+    for i in range(0, len(player_id)):
+        await ctx.send(player_id[i])
 
 
 client.run(config['TOKEN'])
---
+++ bot_classes.py
@@ -0,0 +1,79 @@
+import requests
+
+class statsdata:
+    """Данные полученные с помощью API FACEIT"""
+
+    def __init__(self, api_id, url_base):
+        """В этой функции заполняем парметры headers для cURL и инициализируем base_url и api_token
+        """
+
+        self.api_token = api_id
+        self.base_url = url_base
+
+        self.headers = {
+            'accept': 'application/json',
+            'Authorization': 'Bearer {}'.format(self.api_token)
+        }
+
+    def player_details(self, nickname=None, game_player_id=None):
+        """В эту функцию передаем параметры nickname от аккаунта в FACEIT и game_player_id от аккаунта в CS:GO.
+           Функция возвращает статистику последнего матча.
+        """
+        # Получим player_id
+        api_url = "{}/players".format(self.base_url)
+        if nickname != None:
+            api_url += "?nickname={}".format(nickname)
+        if game_player_id != None:
+            if nickname != None:
+                api_url += "&game_player_id={}".format(game_player_id)
+        api_url += "&game={}".format(game)
+        res = requests.get(api_url, headers=self.headers)
+        data = res.json()
+        if res.status_code == 200:
+            player_id = data["player_id"]
+        else:
+            return None
+
+        # Получим последний match_id
+        api_url = "{}/players/{}/history".format(self.base_url, player_id)
+        api_url += "?game={}&offset={}&limit={}".format(game, 0, 1)
+        response = requests.get(api_url, headers=self.headers)
+        data = response.json()
+        if response.status_code == 200:
+            match_id = data["items"][0]["match_id"]
+        else:
+            return None
+        # Получим статистику матча
+        api_url = "{}/matches/{}/stats".format(self.base_url, match_id)
+        response = requests.get(api_url, headers=self.headers)
+        data = response.json()
+        teams = data["rounds"][0]["teams"]
+        counter = -1
+        stata = []
+        if response.status_code == 200:
+            for team_index in range(0, 2):
+                for gamer_index in range(0, 5):
+                    nick = teams[team_index]["players"][gamer_index]["nickname"]  # никнейм с фэйсит
+                    if nick == nickname:
+                        gamer_index_for_stats = teams[team_index]["players"][gamer_index]
+                        counter += 1
+                        kills = gamer_index_for_stats["player_stats"]["Kills"]
+                        assists = gamer_index_for_stats["player_stats"]["Assists"]
+                        deaths = gamer_index_for_stats["player_stats"]["Deaths"]
+                        K_R_Ratio = gamer_index_for_stats["player_stats"]["K/R Ratio"]
+                        MVPs = gamer_index_for_stats["player_stats"]['MVPs']
+                        headshots = gamer_index_for_stats["player_stats"]['Headshots']
+                        K_D_Ratio = gamer_index_for_stats["player_stats"]["K/D Ratio"]
+
+                        stata.append('Kills:' + kills)
+                        stata.append('Assists: ' + assists)
+                        stata.append('Death: ' + deaths)
+                        stata.append('K/R Ratio: ' + K_R_Ratio)
+                        stata.append('K/D Ratio: ' + K_D_Ratio)
+                        stata.append('MVPs: ' + MVPs)
+                        stata.append('Headshots: ' + headshots)
+                if counter > -1:
+                    break
+        else:
+            return None
+        return stata
