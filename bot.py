b3d3e4555621d349dab8908420763654ca2206ba
Ilnaz Savinov <izsavinov@edu.hse.ru>
2022-05-24 12:50:14 +0300

    changing command reminder_2

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ client = commands.Bot(command_prefix=config['prefix'])
 url_base = config['url_base']
 img = discord.File("gs.png")
 
+
 @client.event
 async def on_ready():
     """Сообщение о подключении бота."""
@@ -25,6 +26,7 @@ async def hello(ctx):
     """Приветствие пользователя по команде в Discord."""
     await ctx.send(config['greeting'])
 
+
 @client.command(pass_context=True)
 async def helpme(ctx):
     """Инструкция по пользованию ботом"""
@@ -33,21 +35,23 @@ async def helpme(ctx):
         colour=discord.Colour.orange()
     )
     embed.set_thumbnail(url="attachment://gs.png")
-    embed.add_field(name="Добро пожаловать в GameStart!", value='Здесь вы узнаете как пользоваться этим ботом. Для начала, вы должны быть зар'
-                                   'егистрированы в сервисе FaceIT. Чтобы вызывать команды, вам необходимо прописать точку(".") '
-                                   'и потом только вводить команду.', inline=False)
+    embed.add_field(name="Добро пожаловать в GameStart!",
+                    value='Здесь вы узнаете как пользоваться этим ботом. Для начала, вы должны быть зар'
+                          'егистрированы в сервисе FaceIT. Чтобы вызывать команды, вам необходимо прописать точку(".") '
+                          'и потом только вводить команду.', inline=False)
 
-    embed.add_field(name='Список команд:', value='1.register - команда для регистрации в бота. Необходимо ввести эту команду,'
-                                                 ' затем ник вашего аккаунта в FaceIT\n\n2.reminder - команда, которая напоминает'
-                                                 ' о предстоящих матчах. Для того, чтобы напоминалка заработала, необходимо ввести'
-                                                 ' команду reminder, а затем через пробел ввести время в формате d-H-M-S'
-                                                 '(Например "21-17:03:30"). Эта команда напомнит вам о предстоящем матче за день,'
-                                                 ' за 15 минут, и в момент начала вашего матча.\n\n3.get_match_stats - команда, которая'
-                                                 ' позволяет узнать статистику вашего последнего матча.\n\n4.total_FI_stats - команда, которая'
-                                                 ' позовляет узнать вашу общую статистику ваших матчей, в которые '
-                                                 'вы заходили через FaceIT.\n\n5. delete_my_account - так как в одном сервере, один пользователь'
-                                                 ' может регистрировать только один аккаунт, то эта команда позволяет удалить текущий аккаунт,'
-                                                 ' и зарегистрировать новый.')
+    embed.add_field(name='Список команд:',
+                    value='1.register - команда для регистрации в бота. Необходимо ввести эту команду,'
+                          ' затем ник вашего аккаунта в FaceIT\n\n2.reminder - команда, которая напоминает'
+                          ' о предстоящих матчах. Для того, чтобы напоминалка заработала, необходимо ввести'
+                          ' команду reminder, а затем через пробел ввести время в формате d-H-M-S'
+                          '(Например "21-17:03:30"). Эта команда напомнит вам о предстоящем матче за день,'
+                          ' за 15 минут, и в момент начала вашего матча.\n\n3.get_match_stats - команда, которая'
+                          ' позволяет узнать статистику вашего последнего матча.\n\n4.total_FI_stats - команда, которая'
+                          ' позовляет узнать вашу общую статистику ваших матчей, в которые '
+                          'вы заходили через FaceIT.\n\n5. delete_my_account - так как в одном сервере, один пользователь'
+                          ' может регистрировать только один аккаунт, то эта команда позволяет удалить текущий аккаунт,'
+                          ' и зарегистрировать новый.')
     await ctx.send(embed=embed, file=img)
 
 
@@ -71,7 +75,7 @@ async def reminder(ctx, message: str):
         colour=discord.Colour.orange()
     )
     embed3.set_thumbnail(url="attachment://gs.png")
-    embed3.add_field(name='Напоминание', value='Начинаем!!! Переходите по ссылке https://www.faceit.com/ru/dashboard')
+    embed3.add_field(name='Напоминание', value='Начинаем! Переходите по ссылке https://www.faceit.com/ru/dashboard')
 
     datepattern = '%d-%H:%M:%S'  # Строковый тип
     gamedata = message
@@ -84,7 +88,7 @@ async def reminder(ctx, message: str):
     deltadate = (date.day * 24 * 3600 + date.hour * 3600 + date.minute * 60 + date.second - 3 * 3600) - (
             time_now.day * 24 * 3600
             + time_now.hour * 3600 + time_now.minute * 60 + time_now.second)
-    if(deltadate - 24 * 60 * 60 > 0):
+    if (deltadate - 24 * 60 * 60 > 0):
         await asyncio.sleep(deltadate - 24 * 60 * 60 + 2)
         await ctx.send(embed=embed, file=img)
     if (deltadate - 15 * 60 > 0):
@@ -186,18 +190,25 @@ async def get_match_stats(ctx):
         for i in range(0, len(found_playersid)):
             massive_playersid.append(found_playersid[i][0])
         statsdata_obj = statsdata(config['APIID'], config['url_base'])
-        player_id, nick_max_kd_ratio, max_kd_ratio, nick_max_kills, max_kills, nick_max_headshots, max_headshots, nick_max_mvps, max_mvps, nick_max_assists, max_assists\
+        player_id, nick_max_kd_ratio, max_kd_ratio, nick_max_kills, max_kills, nick_max_headshots, max_headshots, nick_max_mvps, max_mvps, nick_max_assists, max_assists \
             = statsdata.player_details_for_latest_match(statsdata_obj, found_playerid[0][0], massive_playersid)
         for i in range(0, len(player_id)):
             await ctx.send(player_id[i])
-        await ctx.send('Итоги последнего матча:\n Самым эффективным игроком стал ' + nick_max_kd_ratio + ' с kd_ratio, равное ' + str(max_kd_ratio) +
-                       '.\nБольше всех киллов сделал игрок ' + nick_max_kills + ', всего: ' + str(max_kills) + '.\nГлавной звездой стал ' +
-                       nick_max_mvps + '. Всего у него MVP: ' + str(max_mvps) + '.\nЛучшим помощником оказался ' + nick_max_assists + ' всего ассистов у него: '
-                       + str(max_assists) + '.\nИ наконец, больше всех в голову настрелял ' + nick_max_headshots + ', количество headshots равно ' + str(max_headshots))
+        await ctx.send(
+            'Итоги последнего матча:\n Самым эффективным игроком стал ' + nick_max_kd_ratio + ' с kd_ratio, равное ' + str(
+                max_kd_ratio) +
+            '.\nБольше всех киллов сделал игрок ' + nick_max_kills + ', всего: ' + str(
+                max_kills) + '.\nГлавной звездой стал ' +
+            nick_max_mvps + '. Всего у него MVP: ' + str(
+                max_mvps) + '.\nЛучшим помощником оказался ' + nick_max_assists + ' всего ассистов у него: '
+            + str(
+                max_assists) + '.\nИ наконец, больше всех в голову настрелял ' + nick_max_headshots + ', количество headshots равно ' + str(
+                max_headshots))
     else:
         await ctx.send('Вы не регистрировали свой аккаунт')
     cursor.close()
     conn.close()
+
 
 @client.command(pass_context=True)
 async def total_FI_stats(ctx):
@@ -228,7 +239,8 @@ async def on_guild_join(guild):
     """Приветствие пользователей при добавлении бота на сервер"""
     try:
         join_chanell = guild.system_chanell
-        await join_chanell.send('На вашем сервере работает Gamestart! Чтобы изучить работу бота, введите команду .helpme')
+        await join_chanell.send(
+            'На вашем сервере работает Gamestart! Чтобы изучить работу бота, введите команду .helpme')
     except Exception as e:
         await guild.text_chanells[0].send(
             'На вашем сервере работает Gamestart! Чтобы изучить работу бота, введите команду .help')
@@ -256,7 +268,6 @@ async def delete_my_account(ctx):
 
     cursor.close()
     conn.close()
-
 
 
 client.run(config['TOKEN'])
